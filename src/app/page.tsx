a4ef04e3be7fb83104aa84fb9f222ade0a404986
Robert Ruta <robertruta50@gmail.com>
2025-03-18 21:57:36 +0000

    Added padding to logo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen w-full font-[family-name:var(--font-geist-sans)]">
         <img
+          className="p-4"
           src="/bloingo_transp-bg_logo.png"
           alt="Bloingo logo"
           width={500}
