7074e5600a9c25ee94c6043bb9a036ca4b909e3e
Robert Ruta <robertruta50@gmail.com>
2025-03-18 13:34:56 +0000

    Removed invert and added non snotty version.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen w-full font-[family-name:var(--font-geist-sans)]">
         <img
-          className="dark:invert"
-          src="/bloingo.png"
+          src="/bloingo_non-snotty.png"
           alt="Bloingo logo"
           width={500}
           height={500}
