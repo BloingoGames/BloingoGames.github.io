95d2a1158f608178b6ad648e8f0854dd6d6d72e4
Chris <seadonchristopher@gmail.com>
2025-04-17 11:47:27 +0100

    Add basic text underneath logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen w-full font-[family-name:var(--font-geist-sans)]">
-        <img
-          className="p-4"
-          src="/bloingo_transp-bg_logo.png"
-          alt="Bloingo logo"
-          width={500}
-          height={500}
-        />
+    <div className="flex flex-col justify-center items-center h-screen w-full font-[family-name:var(--font-geist-sans)]">
+      <img
+        className="p-4"
+        src="/bloingo_transp-bg_logo.png"
+        alt="Bloingo logo"
+        width={500}
+        height={500}
+      />
+      <div className="flex w-1/2 justify-center items-center py-6">
+        <p className="text-center">
+          We are Bloingo Games. Check back here for news on the development of
+          our upcoming debut title, Just Snap. We look foward to sharing our
+          progress with you all.
+        </p>
+      </div>
     </div>
   );
 }
