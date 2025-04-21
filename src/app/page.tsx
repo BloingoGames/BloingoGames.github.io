f6335d12ce5c319b91f5e23b68e398ec1bc7591c
Robert Ruta <robertruta50@gmail.com>
2025-04-21 14:22:08 +0200

    Updated asset paths. Changed background to deep-purple.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen px-4 md:px-[15%] font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-[var(--color-deep-purple)] flex flex-col items-center min-h-screen px-4 md:px-[15%] font-[family-name:var(--font-geist-sans)]">
       <img
         className="absolute p-4 self-start w-[30vw] md:w-[20vw]"
-        src="/bloingo_transp-bg_logo.png"
+        src="/assets/bloingo_transp-bg_logo.png"
         alt="Bloingo logo"
       />
 
       <div className="flex justify-center items-center w-full sm:w-3/4 md:w-[50vw] mt-12">
         <img
           className="p-4 w-full h-auto"
-          src="/logo_just-snap_transp.png"
+          src="/assets/logo_just-snap_transp.png"
           alt="Just Snap logo"
         />
       </div>
 
       <div className="flex items-center justify-center w-full sm:w-3/4 md:w-[60vw] py-6">
         <button
-          className="bg-[url('/special-button-orange.svg')] 
+          className="bg-[url('/assets/special-button-orange.svg')] 
                      bg-no-repeat bg-center bg-auto 
                      text-white font-semibold
                      w-full sm:w-[50vw] h-16 sm:h-24
@@ -31,7 +31,7 @@ export default function Home() {
           href="https://www.youtube.com/watch?v=BzxMGlnC2II"
           target="_blank"
           rel="noopener noreferrer"
-          className="block w-full sm:w-[15vw] h-16 sm:h-24 bg-[url('/Youtube-button_1.svg')] bg-no-repeat bg-center bg-auto rounded-lg shadow-md transition duration-150 ease-in-out hover:scale-105"
+          className="block w-full sm:w-[15vw] h-16 sm:h-24 bg-[url('/assets/Youtube-button_1.svg')] bg-no-repeat bg-center bg-auto rounded-lg shadow-md transition duration-150 ease-in-out hover:scale-105"
         >
           <span className="sr-only">Watch on YouTube</span>
         </a>
@@ -39,7 +39,7 @@ export default function Home() {
           href="https://www.kickstarter.com/projects/pn852/bloingo-games-just-snap"
           target="_blank"
           rel="noopener noreferrer"
-          className="block w-full sm:w-[15vw] h-16 sm:h-24 bg-[url('/Kickstarter-button_1.svg')] bg-no-repeat bg-center bg-auto rounded-lg shadow-md transition duration-150 ease-in-out hover:scale-105"
+          className="block w-full sm:w-[15vw] h-16 sm:h-24 bg-[url('/assets/Kickstarter-button_1.svg')] bg-no-repeat bg-center bg-auto rounded-lg shadow-md transition duration-150 ease-in-out hover:scale-105"
         >
           <span className="sr-only">Back our project on Kickstarter</span>
         </a>
@@ -48,7 +48,7 @@ export default function Home() {
           href="https://discord.gg/b6T3MDqW"
           target="_blank"
           rel="noopener noreferrer"
-          className="block w-full sm:w-[15vw] h-16 sm:h-24 bg-[url('/Discord-button_1.svg')] bg-no-repeat bg-center bg-auto rounded-lg shadow-md transition duration-150 ease-in-out hover:scale-105"
+          className="block w-full sm:w-[15vw] h-16 sm:h-24 bg-[url('/assets/Discord-button_1.svg')] bg-no-repeat bg-center bg-auto rounded-lg shadow-md transition duration-150 ease-in-out hover:scale-105"
         >
           <span className="sr-only">Join our Discord community</span>
         </a>
